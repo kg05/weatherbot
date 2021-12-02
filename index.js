770c17948ebec9e279128fdffc3c8ba7f4718653
kg05 <kavishgupta86@gmail.com>
2021-12-02 18:13:26 +0530

    second commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
 
-const dialogflowFulfillment = (req, res) => {
+const dialogflowFulfillment = (request, response) => {
     const agent = WebhookClient({request, response});
 
     function sayHello(){
