33466ac8e7bf7a36d662a1f62d0c841af63b64d0
Kavish Gupta <kavishgupta86@gmail.com>
2021-12-03 12:42:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const {WebhookClient} = require('dialogflow-fulfillment');
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors());
 const port = process.env.PORT || 5000
 
 app.get('/', (request, res) => {
